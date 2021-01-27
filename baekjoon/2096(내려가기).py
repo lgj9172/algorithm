c0d60f979e96b893ca22e41332acabc29ae669f9
GJ <lgj9172@gmail.com>
2021-01-27 16:39:51 +0900

    update baekjoon 2096

--- baekjoon/2096(내려가기).py
+++ baekjoon/2096(내려가기).py
@@ -3,16 +3,12 @@ max_dp = [0, 0, 0]
 min_dp = [0, 0, 0]
 for i in range(N):
     A, B, C = map(int, input().split())
-    if i == 0:
-        max_dp = [A, B, C]
-        min_dp = [A, B, C]
-    else:
-        max_a = max(max_dp[0], max_dp[1]) + A
-        max_b = max(max_dp[0], max_dp[1], max_dp[2]) + B
-        max_c = max(max_dp[1], max_dp[2]) + C
-        min_a = min(min_dp[0], min_dp[1]) + A
-        min_b = min(min_dp[0], min_dp[1], min_dp[2]) + B
-        min_c = min(min_dp[1], min_dp[2]) + C
-        max_dp = [max_a, max_b, max_c]
-        min_dp = [min_a, min_b, min_c]
+    max_a = max(max_dp[0], max_dp[1]) + A
+    max_b = max(max_dp[0], max_dp[1], max_dp[2]) + B
+    max_c = max(max_dp[1], max_dp[2]) + C
+    min_a = min(min_dp[0], min_dp[1]) + A
+    min_b = min(min_dp[0], min_dp[1], min_dp[2]) + B
+    min_c = min(min_dp[1], min_dp[2]) + C
+    max_dp = [max_a, max_b, max_c]
+    min_dp = [min_a, min_b, min_c]
 print(max(max_dp), min(min_dp))
