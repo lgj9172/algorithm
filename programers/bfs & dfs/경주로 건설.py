bcb49e3d8e0c82f700fa84d0a984b6b015886093
GJ <lgj9172@gmail.com>
2021-03-15 23:32:33 +0900

    add solve

---
+++ programers/bfs & dfs/경주로 건설.py
@@ -0,0 +1,73 @@
+from collections import deque
+from copy import deepcopy
+import pprint
+
+
+def solution(board):
+    db = deepcopy(board)
+    queue = deque([(0, 0, "none", 0)])  # x, y, vector, point
+    size = len(db)
+    while queue:
+        x, y, vector, point = queue.popleft()
+        if x < 0 or x > size - 1 or y < 0 or y > size - 1:
+            continue
+        if db[x][y] == 1:
+            continue
+
+        if db[x][y] == 0 or point <= db[x][y]:
+            db[x][y] = point
+        else:
+            continue
+
+        queue.append(
+            (
+                x + 1,
+                y,
+                "east",
+                point + 100 if vector == "none" or vector == "east" else point + 600,
+            )
+        )
+        queue.append(
+            (
+                x - 1,
+                y,
+                "west",
+                point + 100 if vector == "none" or vector == "west" else point + 600,
+            )
+        )
+        queue.append(
+            (
+                x,
+                y + 1,
+                "south",
+                point + 100 if vector == "none" or vector == "south" else point + 600,
+            )
+        )
+        queue.append(
+            (
+                x,
+                y - 1,
+                "north",
+                point + 100 if vector == "none" or vector == "north" else point + 600,
+            )
+        )
+    pprint.pprint
+    (db)
+    answer = db[-1][-1]
+    return answer
+
+
+print(
+    solution(
+        [
+            [0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0, 1],
+            [0, 0, 1, 0, 0, 0, 1, 0],
+            [0, 1, 0, 0, 0, 1, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
+)
