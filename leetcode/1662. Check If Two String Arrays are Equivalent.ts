b453841822233f6f21b5817a638effa836a75a1c
GJ Lee <lgj9172@gmail.com>
2022-10-25 15:31:09 +0900

    fix: solve

--- leetcode/1662. Check If Two String Arrays are Equivalent.ts
+++ leetcode/1662. Check If Two String Arrays are Equivalent.ts
@@ -1,5 +1,3 @@
 function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
-  const string1 = word1.reduce((str, chr) => str + chr, "");
-  const string2 = word2.reduce((str, chr) => str + chr, "");
-  return string1 === string2;
+  return word1.join() === word2.join();
 }
