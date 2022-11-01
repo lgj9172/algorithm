52c98b8baa1f955e0b97730da230f234980efb5d
GJ Lee <lgj9172@gmail.com>
2022-11-01 11:38:34 +0900

    add: solve

---
+++ leetcode/48. Rotate Image.ts
@@ -0,0 +1,10 @@
+function rotate(matrix: number[][]): void {
+  matrix.reverse();
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < i; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+}
